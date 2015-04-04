0589945871e1f479cbf9c97f41f1887b44c84b12
Maxwell Russek <maxwell.russek@gmail.com>
2015-04-04 14:48:02 -0600

    Add global L1 data cache, L1 instruction cache, and L2 cache.

--- cache.c
+++ cache.c
@@ -1,9 +1,37 @@
 /* Cache implementation */
 
+#include "cache.h"
 void init_l1_cache(struct l1_config *l1, struct cache *cache)
 {
 	cache->buf = malloc(l1->l1_cache_size * l1->l1_assoc);
 }
+
+struct cache l1_i = {
+	.block_size = 32,
+	.cache_size = 8192,
+	.assoc = 1,
+	.hit_time = 1,
+	.miss_time = 1,
+	.buf = NULL
+};
+
+struct cache l1_d = {
+	.block_size = 32,
+	.cache_size = 8192,
+	.assoc = 1,
+	.hit_time = 1,
+	.miss_time = 1,
+	.buf = NULL
+};
+
+struct cache l2 = {
+	.block_size = 64,
+	.cache_size = 32768,
+	.assoc = 1,
+	.hit_time = 5,
+	.miss_time = 7,
+	.buf = NULL
+};
 
 void init_l2_cache(struct l2_config *l2, struct cache *cache)
 {
