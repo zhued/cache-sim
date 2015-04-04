fb9978e5491415fa72960e22a95d38c0e29a7480
Maxwell Russek <maxwell.russek@gmail.com>
2015-04-04 14:33:42 -0600

    Add structure to hold memory configuration.

--- config.c
+++ config.c
@@ -2,6 +2,13 @@
 
 #include "config.h"
 
+struct mem_config mem = {
+	.mem_sendaddr = 10,
+	.mem_ready = 30,
+	.mem_chunktime = 15,
+	.mem_chunksize = 8
+};
+
 void load_config(char *path, struct cache *l1, struct cache *l2, int *l2_)
 {
 	return;
--- config.h
+++ config.h
@@ -1,3 +1,11 @@
 /* Configuration structure definition */
 
 void load_config(char *path, struct l1_config *l1, struct l2_config *l2);
+struct mem_config {
+	int mem_sendaddr;
+	int mem_ready;
+	int mem_chunktime;
+	int mem_chunksize;
+};
+
+extern struct mem_config mem;
