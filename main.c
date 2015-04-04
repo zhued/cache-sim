397f24c9948da6bac99606c05b54dc449aa365f0
Maxwell Russek <maxwell.russek@gmail.com>
2015-04-04 14:55:09 -0600

    Update main to update caches with configuration file.

--- main.c
+++ main.c
@@ -4,21 +4,17 @@
 #include <stdlib.h>
 
 #include "config.h"
-
-void load_config(char *path, struct cache *l1, struct cache *l2, 
+#include "cache.h"
 
 int main(int argc, char **argv)
 {
-	struct l1_config l1conf;
-	struct l2_config l2conf;
+	int transfer_time;
+	int bus_width;
 	/* Load configuration from argv[1] if it exists */
 
 	if (argc > 1) {
-		load_config(argv[1], &l1conf, &l2conf);
-	} else {
-		l1conf = l1_default;
-		l2conf = l2_default;
+		load_config(argv[1], &l1_i, &l1_d, &l2, &mem, &transfer_time, &bus_width);
 	}
-	
+
 	return 0;
 }
