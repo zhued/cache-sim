0705ca5558aa066ee16f9e2b5b3a7221cd48becd
Maxwell Russek <maxwell.russek@gmail.com>
2015-04-04 16:19:08 -0600

    Update process trace with arguments and cache statistics structure.

--- main.c
+++ main.c
@@ -6,7 +6,11 @@
 #include "config.h"
 #include "cache.h"
 
-void process_trace(struct cache *l1i, struct cache *l1d, struct cache *l2, struct mem_config *mem)
+void process_trace(struct cache *l1i,
+		   struct cache *l1d,
+		   struct cache *l2,
+		   struct mem_config *mem,
+		   struct stats *st)
 {
 	char op;
 	unsigned long long address;
@@ -20,13 +24,15 @@ int main(int argc, char **argv)
 {
 	int transfer_time;
 	int bus_width;
+
+	struct stats sim_stats;
 	/* Load configuration from argv[1] if it exists */
 
 	if (argc > 1) {
 		load_config(argv[1], &l1_i, &l1_d, &l2, &mem, &transfer_time, &bus_width);
 	}
 
-	process_trace();
+	process_trace(&l1_i, &l1_d, &l2, &mem, &sim_stats);
 
 	return 0;
 }
