f2f04ba61e09d228faef44817971c25ff2633d63
Maxwell Russek <maxwell.russek@gmail.com>
2015-04-11 11:51:20 -0600

    Add everything to testing branch (temporary).

--- main.c
+++ main.c
@@ -2,6 +2,7 @@
 
 #include <stdio.h>
 #include <stdlib.h>
+#include <stdbool.h>
 
 #include "config.h"
 #include "cache.h"
@@ -9,14 +10,24 @@
 void process_trace(struct cache *l1i,
 		   struct cache *l1d,
 		   struct cache *l2,
-		   struct mem_config *mem,
-		   struct stats *st)
+		   struct mem_config *mem)
 {
 	char op;
-	unsigned long long address;
-	unsigned int bytesize;
-	while (scanf("%c %Lx %d\n", &op, &address, &bytesize) == 3) {
-		printf("%c %Lx %d\n", op, address, bytesize);
+	unsigned long address;
+	int bytesize;
+	while (scanf("%c %lx %d\n", &op, &address, &bytesize) == 3) {
+		switch (op) {
+		case 'I':
+			printf("%c %lx %d\n", op, address, bytesize);
+			dispatch_read(l1i, address, bytesize);
+			break;
+		case 'W':
+			dispatch_write(l1d, address, bytesize);
+			break;
+		case 'R':
+			dispatch_read(l1d , address, bytesize);
+			break;
+		}
 	}
 }
 
@@ -25,9 +36,7 @@ int main(int argc, char **argv)
 	int transfer_time;
 	int bus_width;
 
-	struct stats sim_stats;
 	/* Load configuration from argv[1] if it exists */
-
 	if (argc > 1) {
 		load_config(argv[1], &l1_i, &l1_d, &l2, &mem, &transfer_time, &bus_width);
 	}
@@ -36,7 +45,7 @@ int main(int argc, char **argv)
 	init_cache(&l1_d);
 	init_cache(&l2);
 
-	process_trace(&l1_i, &l1_d, &l2, &mem, &sim_stats);
+	process_trace(&l1_i, &l1_d, &l2, &mem);
 
 	return 0;
 }
